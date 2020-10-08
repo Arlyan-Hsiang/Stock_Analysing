16a448bf71acef1bc1a8df8592dc1b934ae00cf6
Hsiang <61619308+Arlyan-Hsiang@users.noreply.github.com>
2020-10-08 20:52:23 +1300

    with header

--- investment.php
+++ investment.php
@@ -13,6 +13,7 @@
 $share = $_POST["shares"];
 $html = "";
 include('simple_html_dom.php');
+//add header
 $opts = array(
   'http'=>array(
     'header'=>"User-Agent:Mozilla/5.0 (iPhone; CPU iPhone OS 7_0 like Mac OS X; en-us) AppleWebKit/537.51.1 (KHTML, like Gecko) Version/7.0 Mobile/11A465 Safari/9537.53\r\n"
